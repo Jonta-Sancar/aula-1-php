4508aacf22a7f393b84f7d39e7b35c0b56be73d8
copo <copoinoxdasilva@gmail.com>
2025-05-24 10:29:35 -0300

    adição do uso de variáveis e concatenação

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Hello World"; // strings valores 
+echo "Olá mundo!!"; // strings valores 
 
 echo "<br>";
 
@@ -20,4 +20,61 @@ var_dump([
   "João",
   0
 ]); // array
+
+echo '<br>';
+
+$nome_da_variavel;
+
+$x = 6;
+
+$y = 8;
+
+
+$soma = $x + $y;
+
+echo $soma;
+
+echo '<br>';
+
+
+$nome = "Zerrai Mundo";
+$data_nascimento = "1970-01-27";
+$endereco = [
+  "Rua comendador",
+  "Brasil zil",
+  367
+];
+
+
+$pessoa = [
+  'nome' => $nome,
+  'data' => $data_nascimento,
+  'endereco' => $endereco
+];
+
+// $array[<chave>]
+
+echo $pessoa['nome'];
+
+echo "<br><br>";
+
+echo $pessoa['endereco'][0];
+
+echo "<br><br>";
+
+/*
+ Zerrai Mundo nasceu em <data>, mora no bairro tal, na rua tal, N° x
+*/
+
+$mensagem = "$nome nasceu em {$pessoa['data']}, mora no bairro {$pessoa['endereco'][1]}, na rua {$pessoa['endereco'][0]}, N° {$pessoa['endereco'][2]}";
+
+$mensagem = $nome . ' nasceu em ' . $pessoa["data"] . ', mora no bairro ' . $pessoa["endereco"][1] . ', na rua ' . $pessoa["endereco"][0] . ', N° ' . $pessoa["endereco"][2];
+
+echo $mensagem;
+
+
+
+
+
+
 
