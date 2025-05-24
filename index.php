644697faa6a08d82a668bc7ce2dec94fb1f3d52c
copo <copoinoxdasilva@gmail.com>
2025-05-24 09:37:10 -0300

    impressão dos tipos de dados básicos no php

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
 
-echo "Hello World!!";
+echo "Hello World"; // strings valores 
 
+echo "<br>";
+
+echo 2025; // int
+echo "<br>";
+echo 10.57; // float
+
+echo "<br>";
+var_dump(false); // bool - verdadeiro e falso
+echo "<br>";
+
+var_dump(null); // null
+
+echo "<br>";
+var_dump([
+  2025,
+  "João",
+  0
+]); // array
 
